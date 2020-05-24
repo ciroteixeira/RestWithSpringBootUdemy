c273fba92deaff0dc18da5ef022767ddb1f6b479
Ciro Teixeira <cteixeira@pdmfc.com>
2020-05-24 13:47:28 +0100

    Implemented Json serialization

---
+++ 08 RestWithSpringBootUdemy - Custom JSON Serialization/src/main/resources/db/migration/V2__PopulateTablePerson.sql
@@ -0,0 +1,3 @@
+insert into person(id, firstname, lastname, address, gender) values
+	(1, 'ciro', 'teixeira', 'rua da industria n20 5e, alverca do ribatejo portugal', 'male'),
+	(2, 'cibele', 'jesus', 'rua otavio soares siqueira n289 bairro cidade nova, nova serrana-mg', 'female');
